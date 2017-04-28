7ea0561887c62a92e8104e10ffb0c450582baead
Unknown <plateau.romain@gmail.com>
2017-04-28 14:49:01 +0200

    Classes Interface et PassagerDAO

---
+++ src/main/java/edu/projet/Interface.java
@@ -0,0 +1,11 @@
+package edu.projet;
+
+import java.util.*;
+
+public interface Interface<T>{
+
+	public List<T> findAll();
+	public T findById (Long id);
+	
+	
+}
---
+++ src/main/java/edu/projet/PassagerDAO.java
@@ -0,0 +1,8 @@
+package edu.projet;
+
+public class PassagerDAO implements Interface<Passager>{
+
+	
+	
+	
+}
