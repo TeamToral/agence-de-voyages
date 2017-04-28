8af0c9b8ddffd518ac0dbdf9da3338fc80c94504
Unknown <plateau.romain@gmail.com>
2017-04-28 11:45:44 +0200

    merge

--- src/main/java/edu/projet/MainAgence.java
+++ src/main/java/edu/projet/MainAgence.java
@@ -1,10 +1,10 @@
-package edu.projet;
-import java.util.*;
-
-public class MainAgence 
-{
-	public static void main (String[] args)
-	{
-		System.out.println("Hello World!");
-	}
-}
+package edu.projet;
+import java.util.*;
+
+public class MainAgence 
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
